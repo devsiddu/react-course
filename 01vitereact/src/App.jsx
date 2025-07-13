ec2c57ba2094b6d60d7b0608c87d25ce9a2d2821
Siddu <siddu124123@gmail.com>
2025-07-13 08:04:29 +0530

    installed npm modules

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1>Starting of project | Codespire</h1>
+      <p>Siddu</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/Codespire.jsx
@@ -0,0 +1,7 @@
+function Codespire (){
+    return (
+        <h1>Hello Codespire</h1>
+    )
+}
+
+export default Codespire
